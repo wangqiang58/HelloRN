3042550edb9e867a5f1c67043c016caf52736f01
wangqiang58 <583233491@qq.com>
2024-12-18 11:19:58 +0800

    feat:调整逻辑
    Signed-off-by: wangqiang58 <583233491@qq.com>

--- android/app/src/main/java/com/hellorn/MainActivity.java
+++ android/app/src/main/java/com/hellorn/MainActivity.java
@@ -1,61 +1,50 @@
 package com.hellorn;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
 
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
-import com.hellorn.bridge.DeviceInfoPackage;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.hellorn.core.DownloadCallback;
+import com.hellorn.core.QPEngineManager;
+import com.hellorn.core.Qp;
+import com.hellorn.core.RNPageActivity;
+import com.hellorn.util.ZipUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MainActivity extends ComponentActivity implements DefaultHardwareBackBtnHandler {
+    private ReactInstanceManager mReactInstanceManager;
+    // 发送消息给 React Native
+    private LifecycleState mLifecycleState
+            = LifecycleState.BEFORE_RESUME;
 
-public class MainActivity extends ReactActivity {
 
     @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegate(this,getMainComponentName()){
-            @Override
-            protected ReactNativeHost getReactNativeHost() {
-                return new ReactNativeHost(MainActivity.this.getApplication()) {
-                    @Override
-                    public boolean getUseDeveloperSupport() {
-                        return BuildConfig.DEBUG;
-                    }
+    protected void onPause() {
+        super.onPause();
+        mLifecycleState = LifecycleState.BEFORE_RESUME;
 
-                    @Nullable
-                    @Override
-                    protected JavaScriptExecutorFactory getJavaScriptExecutorFactory() {
-                        return new HermesExecutorFactory();
-                    }
-
-
-                    @Override
-                    protected List<ReactPackage> getPackages() {
-                        List<ReactPackage> packages = new ArrayList<>();
-                        packages.add(new DeviceInfoPackage());
-                        packages.addAll(new PackageList(this).getPackages());
-                        return packages;
-                    }
-
-                    @Override
-                    protected String getJSMainModuleName() {
-                        return "index";
-                    }
-                };
-            }
-
-        };
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause();
+        }
     }
 
-    @Nullable
     @Override
-    protected String getMainComponentName() {
-        return "home";
+    protected void onResume() {
+        super.onResume();
+
+        mLifecycleState = LifecycleState.RESUMED;
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this);
+        }
     }
 
     @Override
@@ -64,13 +53,83 @@ public class MainActivity extends ReactActivity {
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.destroy();
+        }
+    }
+
+
+    private void sendMessageToReactNative(String message) {
+
+        // 初始化 ReactInstanceManager
+        ReactContext reactContext = mReactInstanceManager.getCurrentReactContext();
+        if (reactContext != null) {
+            reactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("eventName", message);
+        }
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-    }
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        findViewById(R.id.btn4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RNPageActivity.class);
+                intent.putExtra("appKey", "home");
+                intent.putExtra("hybridId", "001");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                QPEngineManager.download("http://192.168.68.128:8000/index.zip", getFilesDir() + "/" + "index.zip", new DownloadCallback() {
+                    @Override
+                    public void onResult(boolean result) {
+                        Log.d("RN", "下载" + result);
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.btn7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ZipUtil.unzipFolder(getFilesDir() + "/" + "index.zip", getFilesDir().getAbsolutePath());
+                boolean result = QPEngineManager.initDB(getFilesDir().getAbsolutePath() + "/rn.db");
+                Log.d("RN", "初始化DB" + result);
+            }
+        });
+
+        findViewById(R.id.btn8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean result = QPEngineManager.insertRecord(getFilesDir().getAbsolutePath() + "/rn.db", "001", 1, getFilesDir() + "/" + "index");
+                Log.d("RN", "插入数据结果:" + result);
+            }
+        });
+
+        findViewById(R.id.btn9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Qp qp = QPEngineManager.queryQp(getFilesDir().getAbsolutePath() + "/rn.db", "001");
+                Log.d("RN", "查询数据结果:" + qp);
+            }
+        });
+    }
 }
